fadcd758886b49b9fe4a53864d2f25c80b0da55b
SirawatVry <139094315+SirawatVry@users.noreply.github.com>
2023-10-22 23:13:05 +0700

    Update fstreamm.cpp

--- fstreamm.cpp
+++ fstreamm.cpp
@@ -8,14 +8,14 @@ void AddStudent(string FN);
 void DisplayStudent(string FN);
 int main()
 {
-	const string Filename = "student.dat";
-	ifstream InFile;
-	ofstream OutFile;
+	const string Filename = "student.dat"; // กำหนดชื่อไฟล์ลงในตัวแปรเพื่อสร้างไฟล์ตามชื่อที่ใส่
+	ifstream InFile; //ใช้อ่านข้อมูลจากไฟล์
+	ofstream OutFile;//ใช้เขียนข้อมูลลงในไฟล์
 	int c;
 	do {
-		system("cls"); // call external command
+		system("cls"); //คำสั่งเคลียร์หน้าจอ ใช้ในการทำให้การแสดงดูง่ายใส่ก็ได้
 		c = Menu();
-		switch(c)
+		switch(c) //menuแบบปกติ
 		{
 			case 1 : AddStudent(Filename); break;
 			case 2 : DisplayStudent(Filename); break;
@@ -42,30 +42,30 @@ int Menu()
 }
 void AddStudent(string FN)
 {
-// open file for write and append
-	ofstream OutFile(FN.c_str(), ios_base::out | ios_base::app);
-	if (OutFile.is_open()) {
-		string Id, Name;
+//ฟังค์ชั่นเปิดเพื่อเขียนต่อ
+	ofstream OutFile/*ประกาศในการจะเขียนข้อมูล*/(FN.c_str()/*แปลงจาก string เป็น char เพื่อให้สามารถเปิดไฟล์โดยตรง*/, ios_base::out | ios_base::app);//ใช้ในการเปิดไฟล์เพื่อเขียนต่อจากที่เขียนไว้
+	if (OutFile.is_open()) { //ถ้าเปิดไฟล์ได้
+		string Id, Name; //ประกาศรับค่าประเภท string ชื่อ Id Name
 		cout << "\n Add Student \n";
 		cout << "Enter id : ";
 		cin >> Id;
 		cout << "Enter name : ";
 		cin >> Name;
-		// write data to file student.dat
-		OutFile << Id << " " << Name << endl;
-		OutFile.close();
+
+		OutFile << Id << " " << Name << endl;//เขียนข้อมูลลงไฟล์
+		OutFile.close();//ปิดโหมดการเขียน
 		char Wait;
-		cin.get(Wait);
+		cin.get(Wait);//รอการรับค่า Enter ในการดำเนินการต่อ
 		cout << "\nSaved already ,Press Enter to continue";
 		cin.get(Wait);
 	}
-	else cout << "File could not opened." << endl;
+	else cout << "File could not opened." << endl;//ถ้าเปิดไฟล์ไม่ได้
 }
 void DisplayStudent(string FN)
 {
-	ifstream InFile(FN.c_str(), ios_base::in); // open file for read
-	if (InFile.is_open()) {
-		string Id, Name;
+	ifstream InFile(FN.c_str(), ios_base::in); //เปิดไฟล์ในโหมดอ่าน
+	if (InFile.is_open()) {//เช็คการเปิดของไฟล์
+		string Id, Name;//กำหนดค่า
 		string line(30,'=');
 		int n = 0;
 		cout << "\nList Student\n";
@@ -73,15 +73,15 @@ void DisplayStudent(string FN)
 		cout << " No. Id Name \n";
 		cout << line << endl;
 // read data from file student.dat
-	InFile >> Id >> Name;
-	while (!InFile.eof()) {
+	InFile >> Id >> Name;//อ่านข้อมูล Id และ Name
+	while (!InFile.eof()) {  //ลูปในการอ่านข้อมูล Id และ Name จนกว่าข้อมูลจะหมด
 		n = n + 1;
 		cout << right << setw(3) << n << " : ";
 		cout << left << setw(6) << Id;
 		cout << " " << Name << endl;
-		InFile >> Id >> Name;
+		InFile >> Id >> Name;//อ่านข้อมูลจากไฟล์แล้วนำไปเก็บใส่ตัวแปร Id และ Name
 	}
-	InFile.close();
+	InFile.close(); //ปิดไฟล์ในโหมดการอ่าน
 	char Wait;
 	cin.get(Wait);
 	cout << "\nPress Enter to continue";
